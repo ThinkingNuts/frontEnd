e00d5efb09fcc1e64e9df4bd88df3dcf0813bb35
wuqichao <1163085878@qq.com>
2017-08-21 18:14:00 +0800

    项目初始化

---
+++ dj-front/src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id='login'>
+    <el-input icon='pwd'>
+
+    </el-input>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
